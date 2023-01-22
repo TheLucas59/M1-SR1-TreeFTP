f9529f061c6f3cf02b2f99f05572d97a1d9a8652
Lucas Plé <lucas.ple.etu@univ-lille.fr>
2023-01-22 17:37:58 +0100

    FIX now correctly handling program parameters and rename main TreeFTP class to Main class

---
+++ src/main/java/com/lucasple/treeftp/app/Main.java
@@ -0,0 +1,59 @@
+package com.lucasple.treeftp.app;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.lucasple.treeftp.communication.FTPClient;
+import com.lucasple.treeftp.exceptions.CommandFailedException;
+
+/**
+ * Main class that launches the TreeFTP command
+ * @author Lucas Plé
+ *
+ */
+
+public class Main {
+	
+	private static final Log LOGGER = LogFactory.getLog(Main.class);
+	
+	private static final int FTP_PORT = 21;
+	private static final int DEFAULT_DEPTH = 99;
+	
+	private static final String USER_OPTION = "-u";
+	private static final String PASSWORD_OPTION = "-p";
+	private static final String DEPTH_OPTION = "-d";
+
+	
+    public static void main(String[] args) {
+    	String address = "";
+    	String user = "";
+    	String password = "";
+    	int maxDepth = DEFAULT_DEPTH;
+    	
+    	if(args.length > 0) {
+    		address = args[0];
+	    	for(int i = 1; i < args.length; i++) {
+	    		switch(args[i]) {
+	    			case USER_OPTION:
+	    				user = args[++i];
+	    				break;
+	    			case PASSWORD_OPTION :
+	    				password = args[++i];
+	    				break;
+	    			case DEPTH_OPTION :
+	    				maxDepth = Integer.parseInt(args[++i]);
+	    				break;
+	    			default :
+	    				LOGGER.error("The option " + args[i] + " does not exist.");
+	    				System.exit(1);
+	    		}
+	    	}
+    	}
+    	
+		try {
+			FTPClient.handle(address, FTP_PORT, user, password, maxDepth);
+		} catch (CommandFailedException e) {
+			LOGGER.error("Error during the FTP process", e);
+		}
+    }
+}
